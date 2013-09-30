f421291139a3ab80d03cdbfe2be8aeac2fb4472e
Esteban Valery <evalery@dridco.com>
2013-09-30 12:45:48 -0300

    Cambios varios
    -Se agrega una nueva dependencia, file 0.2.1
    -Nuevo algoritmo, levanta el bean original en base al parametro --i=[filepath], luego parsea el archivo y detecta package, class y private fields.
    -Nuevo parametro opcional --s=[suffix], por default es Fixture
    -Pregunta antes de sobreescribir en caso de que exista el output file.
    -Posiciona el output file (fixture) sobre el mismo package que el bean original pero bajo la carpeta src/test/java.

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 var argv = require('optimist')
-	.usage('Usage: $0 --input=[filename] --fixture=[string] --bean=[string]')
-    .demand(['fixture', 'bean'])
+	.usage('Usage: $0 --i=[filepath] --s=[optional suffix]')
+    .demand(['i'])
     .argv;
-
-var fields = require('./freader.js');
 var fs = require('fs');
+
+var parsed = require('./freader.js');
+
+var fixtureClassName = parsed.fixture;
+var beanClassName = parsed.clazz;
+
+var outputFile = parsed.outputFolder+'/'+fixtureClassName+'.java';
+if (fs.existsSync(outputFile)) {
+	process.stdin.resume();
+	process.stdin.setEncoding('utf8');
+	existsQuestion();
+} else {
+	doWork();
+}
+
+function existsQuestion() {
+	console.log('Output file exists: ' + outputFile);
+	console.log('Override? (y/n): ');
+	process.stdin.once('data', function (chunk) {
+		if (chunk && chunk.trim() == 'y') {
+			doWork();
+			process.exit(0);
+		} else if (chunk && chunk.trim() == 'n') {
+			console.log('Aborted');
+			process.exit(0);
+		} else {
+			existsQuestion();
+		}
+	});
+}
 
 function capitalize(word) {
 	return word.charAt(0).toUpperCase() + word.slice(1);
@@ -26,48 +54,47 @@ function toConstantStyle(camelized) {
 	return constant.toUpperCase();
 }
 
-var fixtureClassName = argv.fixture;
-var beanClassName = argv.bean;
+function doWork() {
+	var output = 'package '+parsed.package+';\n\n';
 
-var output = 'public class '+fixtureClassName+' {\n\n';
+	output += 'public class '+fixtureClassName+' {\n\n';
 
-fields.forEach(function(field){
-	output += '\tprivate static final ' + field.type + ' DEFAULT_' + toConstantStyle(field.name) + ' = /initial value/;\n';
-});
-output += '\n';
+	parsed.fields.forEach(function(field){
+		output += '\tprivate static final ' + field.type + ' DEFAULT_' + toConstantStyle(field.name) + ' = /initial value/;\n';
+	});
+	output += '\n';
 
-output += '\tprivate '+beanClassName+' myBean;\n\n';
+	output += '\tprivate '+beanClassName+' myBean;\n\n';
 
-output += '\tpublic static '+fixtureClassName+' New() {\n';
-output += '\t\treturn new '+fixtureClassName+'();\n';
-output += '\t}\n\n';
-
-output += '\tprivate '+fixtureClassName+'() {\n';
-output += '\t\tmyBean = new '+beanClassName+'();\n';
-fields.forEach(function(field){
-	output += '\t\twith'+capitalize(field.name)+'(DEFAULT_'+toConstantStyle(field.name)+');\n';
-});
-output += '\t}\n\n';
-
-output += '\tpublic '+beanClassName+' build() {\n';
-output += '\t\treturn myBean;\n';
-output += '\t}\n\n';
-
-fields.forEach(function(field){
-	output += '\tpublic '+fixtureClassName+' with'+capitalize(field.name)+'('+field.type+' '+field.name+') {\n';
-	output += '\t\tmyBean.set'+capitalize(field.name)+'('+field.name+');\n';
-	output += '\t\treturn this;\n';
+	output += '\tpublic static '+fixtureClassName+' New() {\n';
+	output += '\t\treturn new '+fixtureClassName+'();\n';
 	output += '\t}\n\n';
-});
 
-output += '}';
+	output += '\tprivate '+fixtureClassName+'() {\n';
+	output += '\t\tmyBean = new '+beanClassName+'();\n';
+	parsed.fields.forEach(function(field){
+		output += '\t\twith'+capitalize(field.name)+'(DEFAULT_'+toConstantStyle(field.name)+');\n';
+	});
+	output += '\t}\n\n';
 
-var outputFolder = './output';
-if (!fs.existsSync(outputFolder)) {
-	fs.mkdirSync(outputFolder);
+	output += '\tpublic '+beanClassName+' build() {\n';
+	output += '\t\treturn myBean;\n';
+	output += '\t}\n\n';
+
+	parsed.fields.forEach(function(field){
+		output += '\tpublic '+fixtureClassName+' with'+capitalize(field.name)+'('+field.type+' '+field.name+') {\n';
+		output += '\t\tmyBean.set'+capitalize(field.name)+'('+field.name+');\n';
+		output += '\t\treturn this;\n';
+		output += '\t}\n\n';
+	});
+
+	output += '}';
+
+	if (!fs.existsSync(parsed.outputFolder)) {
+		console.log('Making output folder: '+parsed.outputFolder);
+		file.mkdirsSync(parsed.outputFolder);
+	}
+
+	fs.writeFileSync(outputFile, output);
+	console.log('File was written: '+outputFile);
 }
-var outputFile = outputFolder+'/'+fixtureClassName+'.java';
-
-fs.writeFileSync(outputFile, output);
-//console.log(output);
-console.log('File was written: '+outputFile);
--- freader.js
+++ freader.js
@@ -1,21 +1,67 @@
 var argv = require('optimist').argv;
 var fs = require('fs');
+var file = require('file');
 
-var inputFile = argv.i || 'input';
+function capitalize(word) {
+	return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+var inputFile = file.path.abspath(argv.i);
+var suffix = capitalize(argv.s || 'Fixture');
+
+console.log('Input file is: ' + inputFile);
+console.log('Using suffix: ' + suffix);
 
 var fileData = fs.readFileSync(inputFile, {encoding : 'utf8'});
 
-// PRIVATE_FIELD_PATTERN = /^\s*private\s*(\w+)\s*(\w+)\s*[^(]*;\s*$/;
-	
+PACKAGE_PATTERN = /^\s*package\s+(.+);$/;
+CLASS_PATTERN = /.*\s+class\s+(\w+).*$/;
+PRIVATE_FIELD_PATTERN = /^\s*private\s*(\w+)\s*(\w+)\s*.*;\s*$/;
+STATIC_FIELD_PATTERN = /.*static.*/;
+
+var package;
+var clazz;
+
 var fields = [];
 fileData.split('\n').forEach(function(line, index){
 	line = line.trim();
-	var lineParts = line.split(/\s+/);
-	console.log('Processing [' + lineParts + ']');
-	if (lineParts.length != 2) {
-		throw 'Invalid input line ' + (index+1) + ' = ' + line;
+	if (!package) {
+		var matchesPackage = line.match(PACKAGE_PATTERN);
+		if (matchesPackage) {
+			package = matchesPackage[1];
+		}
 	}
-	fields.push({type : lineParts[0], name : lineParts[1]});
+	if (!clazz) {
+		var matchesClass = line.match(CLASS_PATTERN);
+		if (matchesClass) {
+			clazz = matchesClass[1];
+		}
+	}
+	var matchesPrivateField = line.match(PRIVATE_FIELD_PATTERN);
+	if (matchesPrivateField && !line.match(STATIC_FIELD_PATTERN)) {
+		fields.push({type : matchesPrivateField[1], name : matchesPrivateField[2]});
+	}
 });
 
-module.exports = fields;
+console.log('Package detected: ' + package);
+console.log('Class detected: ' + clazz);
+console.log('Fields detected: ');
+console.log(fields);
+
+var packageLevels = package.split('.');
+
+var iFileTokens = inputFile.split('/');
+if (iFileTokens[0] == '') {
+	iFileTokens = iFileTokens.slice(1);
+}
+
+var oFileTokens = iFileTokens.slice(0, iFileTokens.length-packageLevels.length-3);
+oFileTokens.push('test', 'java');
+oFileTokens = oFileTokens.concat(packageLevels);
+
+var outputFolder = '';
+oFileTokens.forEach(function(token){
+	outputFolder += '/' + token;
+});
+
+module.exports = {fields : fields, clazz : clazz, fixture: clazz+suffix, package : package, outputFolder: outputFolder};
